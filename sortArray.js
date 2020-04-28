1320dc25ff72cffae8e33e6da45204b74ba1dea0
jackdo69 <jackie.duc.do@gmail.com>
2020-04-28 08:41:15 +0700

    bubble sort Array

---
+++ sortArray.js
@@ -0,0 +1,16 @@
+const sortArray = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j + 1] < arr[j]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+const arr = [2, 16, 7, 44, 5, 39];
+
+console.log(sortArray(arr));
